85752c474ba320032d2d3d030c0331ea943d98cd
keiichiro <keiichiro.material@gmail.com>
2021-05-12 15:06:49 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/questions', to: 'static_pages#questions'
   get '/signup', to: 'users#new'
+  resources :users
 end
