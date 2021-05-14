18d5456b871ce24feb6acea4914b630329115586
keiichiro <keiichiro.material@gmail.com>
2021-05-14 21:46:36 +0900

    add reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "アカウントの有効化(Spring)"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "パスワードの再設定(Spring)"
   end
 end
