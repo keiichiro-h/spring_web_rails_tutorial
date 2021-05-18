06083826e561453f8050c0c59c5426b1b8296efe
keiichiro <keiichiro.material@gmail.com>
2021-05-18 18:14:27 +0900

    modify SMTP config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nameless-cliffs-31001.herokuapp.com'
+  host = 'infinite-fortress-40025.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
